81d6246878dffb6f8a9196027ceca9191eba9584
Esther Olatunde <olatundeesther@gmail.com>
2018-09-13 03:10:41 +0100

    Adds unit tests for completed and uncompleted scopes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,4 +13,32 @@ RSpec.describe Task, type: :model do
   it 'has an invalid data' do
     expect(build(:task, description: nil)).to_not be_valid
   end
+
+  describe '.completed' do
+    it 'returns completed tasks' do
+      task1 = FactoryBot.create(:task, status: 'completed')
+      task2 = FactoryBot.create(:task, status: 'uncompleted')
+      task3 = FactoryBot.create(:task, status: 'completed')
+
+      tasks = Task.completed
+
+      expect(tasks).to include(task1)
+      expect(tasks).to include(task3)
+      expect(tasks).to_not include(task2)
+    end
+  end
+
+  describe '.uncompleted' do
+    it 'returns uncompleted tasks' do
+      task1 = FactoryBot.create(:task, status: 'uncompleted')
+      task2 = FactoryBot.create(:task, status: 'uncompleted')
+      task3 = FactoryBot.create(:task, status: 'completed')
+
+      tasks = Task.uncompleted
+
+      expect(tasks).to include(task1)
+      expect(tasks).to include(task2)
+      expect(tasks).to_not include(task3)
+    end
+  end
 end
